156d86223aa38a8412e7ecae3589bd05a95f70ef
Steffen Vutborg <svu@ucn.dk>
2016-02-12 09:15:03 +0100

    Included The Say example

---
+++ SayAndAsk/SayAndAsk/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SayAndAsk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string Answer;
+            Say("Hello");
+            // Answer = Ask("Are you haveing a nice day?");
+            Repeat("Are you haveing a nice day?");
+            
+
+            Console.Read();
+        }
+
+        static void Repeat(string question)
+        {
+            Say(Ask(question));
+        }
+
+        static string Ask(string question)
+        {
+            Say(question);
+            return Console.ReadLine();
+        }
+
+        static void Say(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
