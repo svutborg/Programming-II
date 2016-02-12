e90c5c4ceb6e86889316fb1da0d5c73872c5257c
Steffen Vutborg <svu@ucn.dk>
2016-02-12 10:30:03 +0100

    added example with structures and enum

---
+++ EnumMonths/EnumMonths/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EnumMonths
+{
+    class Program
+    {
+        enum Months { January, February, Martch, April, May, June,
+            July, August, September, October, November, December};
+
+        static void Main(string[] args)
+        {
+            foreach(string m in Enum.GetNames(typeof(Months)))
+            {
+                Console.WriteLine(m);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ StructHours/StructHours/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StructHours
+{
+    struct Hours
+    {
+        int hours;
+        int minutes;
+        int seconds;
+
+        public Hours(int hh, int mm, int ss)
+        {
+            hours = hh + mm/60;
+            minutes = mm%60 + ss/60;
+            seconds = ss%60;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0,2:00}:{1,2:00}:{2,2:00}", hours, minutes, seconds);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hours time = new Hours(40, 62, 26);
+
+            Console.WriteLine(time.ToString());
+            Console.Read();
+        }
+    }
+}
