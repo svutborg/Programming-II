ef10196bf8f6b2887ce86cca34d46016ebfb8282
Steffen Vutborg <SVU@ucn.dk>
2016-02-22 09:27:51 +0100

    Added a static auto property to Circle Class

--- CircleObject/CircleObject/Circle.cs
+++ CircleObject/CircleObject/Circle.cs
@@ -12,6 +12,11 @@ namespace CircleObject
         double circumference;
         double area;
 
+        static public int CircleCounter
+        {
+            get; private set;
+        }
+
         public int Radius
         {
             get
@@ -40,6 +45,7 @@ namespace CircleObject
             radius = 0;
             circumference = 0;
             area = 0;
+            CircleCounter++;
         }
 
         public Circle(int r)
@@ -47,6 +53,7 @@ namespace CircleObject
             radius = r;
             circumference = 2 * Math.PI * radius;
             area = Math.PI * radius * radius;
+            CircleCounter++;
         }
     }
 }
--- CircleObject/CircleObject/Program.cs
+++ CircleObject/CircleObject/Program.cs
@@ -8,8 +8,15 @@ namespace CircleObject
         {
             Circle C1 = new Circle(5);
             Console.WriteLine(C1.Area);
+
+            Console.WriteLine($"Number of objects {Circle.CircleCounter}");
+
             C1.Radius = 4;
             Console.WriteLine(C1.Area);
+
+            Circle C2 = new Circle();
+            Console.WriteLine($"Number of objects {Circle.CircleCounter}");
+
             Console.Read();
         }
     }
