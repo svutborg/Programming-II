4436531ece9d3fdf7b3c44e9cdfa5aa40d525d3f
Steffen Vutborg <SVU@ucn.dk>
2016-04-06 09:13:05 +0200

    Racing threads without delegates

--- RacingThreads/RacingThreads/Program.cs
+++ RacingThreads/RacingThreads/Program.cs
@@ -12,14 +12,16 @@ namespace RacingThreads
         delegate void ThreadDelegate();
         static void Main(string[] args)
         {
-            ThreadDelegate TD = new ThreadDelegate(ThreadMethod);
-            ThreadStart TS = new ThreadStart(TD);
+
+            //ThreadDelegate TD = new ThreadDelegate(ThreadMethod);
+            //ThreadStart TS = new ThreadStart(TD);
+
             for (int i = 0; i < 4; i++)
             {
-                Thread T = new Thread(TS);
+                Thread T = new Thread(ThreadMethod);
                 T.Name = String.Format($"T{i}");
                 T.IsBackground = true;
-                T.Start();
+                T.Start(100*i);
                 if (i==2)
                 {
                     T.Priority = ThreadPriority.Lowest;
@@ -39,10 +41,14 @@ namespace RacingThreads
                 }
             }
         }
-
-        static void ThreadMethod()
+        static void OtherMethod(string mes)
         {
-            int num = 0;
+            Console.WriteLine(mes);
+        }
+
+        static void ThreadMethod(object arg)
+        {
+            int num = (int)arg;
             string name = Thread.CurrentThread.Name;
             int ThreadNumber = int.Parse(name.Substring(1, 1));
             string indent = "";
